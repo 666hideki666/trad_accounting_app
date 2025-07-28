fcc9b1e9d02be72387280b1d55d7b846b489581e
hideki <hideki6k@gmail.com>
2025-07-28 15:42:28 +0900

    feat: Update menu data

--- menu/menu.js
+++ menu/menu.js
@@ -9,11 +9,14 @@ const menuData = {
         { name: "プレーン", price: 180 },
         { name: "チョコチップ", price: 250 },
         { name: "シトラス", price: 270 },
-        { name: "抹茶チョコチップ", price: 270 }
+        { name: "抹茶ブルーベリー", price: 270 }
     ],
     "名水ソーダ": [
         { name: "ゴールデンキウイ", price: 400 },
         { name: "パイナップル", price: 400 },
         { name: "クラフトコーラ", price: 400 }
+    ],
+    "トッピング": [
+        { name: "アイスクリームトッピング", price: 50 }
     ]
 };
